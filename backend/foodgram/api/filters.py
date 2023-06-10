eb56e4d0e661f1f899f5f3dd2c5fa4ed549aa189
Алексей Акимов <akimov_alexey98@mail.ru>
2023-06-10 17:10:53 +0600

    update

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -25,7 +25,7 @@ class RecipeFilter(FilterSet):
     )
 
     is_favorited = filters.BooleanFilter(method='filter_is_favorite')
-    is_in_purchases_list = filters.BooleanFilter(
+    is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_purchases_list'
     )
 
@@ -37,10 +37,10 @@ class RecipeFilter(FilterSet):
         user = self.request.user
         if value and not user.is_anonymous:
             return queryset.filter(favorites__user=user)
-        return queryset
+        return queryset.none()
 
     def filter_is_in_purchases_list(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
             return queryset.filter(shopping_cart__user=user)
-        return queryset
+        return queryset.none()
